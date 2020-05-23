17cb719cf77aa74b469a34c71b4ef633659d3130
olivier.bonheur <olivier.bonheur@gmail.com>
2020-05-23 16:29:47 +0200

    Project launch

---
+++ src/main/java/com/safetynet/application/SafetyNetApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetApplication.class, args);
+	}
+
+}
